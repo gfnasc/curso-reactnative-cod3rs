6d5d48ade916b67c6bca8d5d56a27fc9cc13c035
Guilherme Nascimento <guilherme.freitasnascimento@gmail.com>
2020-06-21 01:59:20 -0300

    entendo import e export

--- src/components/Simples.js
+++ src/components/Simples.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import Padrao from '../styles/Padrao'
-import Parimpar from '../components/Parimpar'
+import Parimpar from './Parimpar'
+import { Inverter, MegaSena } from './Multi'
 
 export default (props) => {
     return (
         <View>
             <Text style={Padrao.ex}>{props.texto}</Text>
             <Parimpar numero={33}></Parimpar>
+            <Inverter texto="React Native"></Inverter>
+            <MegaSena></MegaSena>
         </View>
     );
 }
